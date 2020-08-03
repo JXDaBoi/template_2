dc5570737d2d04e46973f58ad5e412c5abe4bbbd
JXDaBoi <jingxiangdaboi@gmail.com>
2020-08-03 23:48:38 +0800

    Added Interaction

--- script/app.js
+++ script/app.js
@@ -1,15 +1,17 @@
+var name;
+
 const one = document.getElementById("one");
 const two = document.getElementById("two");
 const three = document.getElementById("three");
 
 one.addEventListener("click", function (e) {
-  one.classList.add("invisible");
+  one.style.display = "none";
 });
 
 two.addEventListener("click", function (e) {
-  two.classList.add("invisible");
+  two.style.display = "none";
 });
 
 three.addEventListener("click", function (e) {
-  three.classList.add("invisible");
+  three.style.display = "none";
 });
